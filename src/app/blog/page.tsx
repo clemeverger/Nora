a3dad51366c5d56608f21003f47b04d9dddbabc0
Clément Verger <clemeverger@gmail.com>
2023-06-20 13:59:20 +0100

    🐛 Fix theme

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,15 +1,62 @@
 import React from 'react'
+import Image from 'next/image'
+
+const Article1 = () => {
+  return (
+    <article className='bg-yellow rounded-lg p-4'>
+      <h4 className='text-xl font-semibold text-primary_dark'>Médecine</h4>
+      <p className='text-primary_dark font-medium'>Découvre notre liste de spécialistes spécialement adaptés pour ton besoin</p>
+    </article>
+  )
+}
+
+const Article2 = () => {
+  return (
+    <article>
+      <h4>Médecine</h4>
+      <p>Découvre notre liste de spécialistes spécialement adaptés pour ton besoin</p>
+    </article>
+  )
+}
+
+const Article3 = () => {
+  return (
+    <article>
+      <h4>Médecine</h4>
+      <p>Découvre notre liste de spécialistes spécialement adaptés pour ton besoin</p>
+    </article>
+  )
+}
+
+const Article4 = () => {
+  return (
+    <article>
+      <h4>Médecine</h4>
+      <p>Découvre notre liste de spécialistes spécialement adaptés pour ton besoin</p>
+    </article>
+  )
+}
 
 const Blog = () => {
   return (
-    <div>
-      <h3 className='text-3xl'>Blog</h3>
-      <input
-        type={'text'}
-        className='bg-primary_dark rounded-full px-4 py-1'
-        placeholder={'Rechercher un article'}
-      />
-    </div>
+    <>
+      <h3 className='text-3xl text-primary_dark mb-4 mt-4'>Blog</h3>
+      <div className='flex gap-2 items-center rounded-full px-4 bg-[#F2E5F9] bg-opacity-40 rounded-fullw-full'>
+        <Image
+          src={'/search.svg'}
+          alt={'search'}
+          width={20}
+          height={20}
+        />
+        <input
+          className='bg-transparent w-full py-2  placeholder-primary_dark'
+          type={'text'}
+          placeholder={'Rechercher un article'}
+        />
+      </div>
+      <Article1 />
+      <Article2 />
+    </>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,40 +3,18 @@
 module.exports = {
   content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
-    },
-    colors: {
-      primary: '#E3B3FA',
-      primary_dark: '#3A2544',
-      primary_light: '#F2E5F9',
-      white: '#FDFDFD',
-      yellow: '#FCECC2',
-      orange: '#FAEBE3',
-    },
-    fontFamily: {
-      primary: ['"Rametto one"', 'serif'],
-      secondary: ['Manrope', 'serif'],
-    },
-    fontSize: {
-      sm: '0.8rem',
-      base: '1rem',
-      xl: '32',
-      '2xl': '1.563rem',
-      '3xl': '1.953rem',
-      '4xl': '2.441rem',
-      '5xl': '3.052rem',
-    },
     extend: {
-      spacing: {
-        128: '32rem',
-        144: '36rem',
+      colors: {
+        primary: '#E3B3FA',
+        primary_dark: '#3A2544',
+        primary_light: '#F2E5F9',
+        white: '#FDFDFD',
+        yellow: '#FCECC2',
+        orange: '#FAEBE3',
       },
-      borderRadius: {
-        '4xl': '2rem',
+      fontFamily: {
+        primary: ['"Rametto one"', 'serif'],
+        secondary: ['Manrope', 'serif'],
       },
     },
   },
