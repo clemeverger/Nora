de5a058b4993325c92723a09dc9d9a3c32e9099c
Clément Verger <clemeverger@gmail.com>
2023-06-22 08:45:20 +0100

    Articles

--- src/app/blog/conseils/components/Articles.tsx
+++ src/app/blog/conseils/components/Articles.tsx
@@ -1,27 +1,45 @@
 import React from 'react'
 
-const options = [
-  { value: 'all', label: 'Tous' },
-  { value: 'Gynécologue obstétricien', label: 'Gynécologue' },
-  { value: 'Médecin généraliste', label: 'Médecin généraliste' },
-  { value: 'Sage-femme', label: 'Sage-femme' },
+const ARTICLES = [
+  {
+    title: 'Alimentation et Endométriose: Conseils pour soulager les symptômes',
+    image: '/article1.png',
+  },
+  {
+    title: "Impact émotionnel de l'endométriose : soutien psychologique et bien-être",
+    image: '/article2.png',
+  },
+  {
+    title: 'Les avancées récentes dans la recherche sur les troubles',
+    image: '/article3.png',
+  },
+  {
+    title: 'Nouveautés dans la recherche sur le SOPK : avancées et découvertes récentes',
+    image: '/article4.png',
+  },
+  {
+    title: "Impact émotionnel de l'endométriose : soutien psychologique et bien-être",
+    image: '/article5.png',
+  },
+  {
+    title: 'Les avancées récentes dans la recherche sur les troubles',
+    image: '/article6.png',
+  },
+  {
+    title: 'Nouveautés dans la recherche sur le SOPK : avancées et découvertes récentes',
+    image: '/article7.png',
+  },
+  {
+    title: "Impact émotionnel de l'endométriose : soutien psychologique et bien-être",
+    image: '/article8.png',
+  },
 ]
 
 const Articles = () => {
   return (
     <section className='text-xl text-primary_dark'>
       Articles
-      <div>
-        {options.map((option) => (
-          <button
-            className={'p-1 rounded text-secondary bg-primary text-opacity-100'}
-            key={option.value}
-            value={option.value}
-          >
-            {option.label}
-          </button>
-        ))}
-      </div>
+      <div></div>
     </section>
   )
 }
